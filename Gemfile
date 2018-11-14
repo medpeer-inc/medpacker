6caaa4eea67b5d12a315b475557d54896eb51154
pipopotamasu <ec0156hx@gmail.com>
2018-11-15 00:03:15 +0900

    spec

--- Gemfile
+++ Gemfile
@@ -56,6 +56,8 @@ group :test do
   gem 'selenium-webdriver'
   # Easy installation and use of chromedriver to run system tests with Chrome
   gem 'chromedriver-helper'
+  gem 'rspec-rails'
+  gem 'factory_bot_rails'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
---
+++ spec/helpers/webpack_bundle_helper.rb
@@ -0,0 +1,121 @@
+require 'rails_helper'
+
+RSpec.describe WebpackBundleHelper, type: :helper do
+  before do
+    manifest = {
+      "item_group_editor.css": '/packs/item_group_editor-5d7c7164b8a0a9d675fad9ab410eaa8d.css',
+      "item_group_editor.js": '/packs/item_group_editor-857e5bfa272e71b6384046f68ba29d44.js',
+      "item_group_editor.js.map": '/packs/item_group_editor.js.map',
+      "union-ok.png": '/packs/union-ok-857e5bfa272e71b6384046f68ba29d44.png',
+      "union-ok@2x.png": '/packs/union-ok@2x-5d7c7164b8a0a9d675fad9ab410eaa8d.png'
+    }.stringify_keys
+
+    allow_any_instance_of(WebpackBundleHelper).to receive(:manifest).and_return(manifest)
+  end
+
+  describe '#asset_bundle_path' do
+    context 'given existing *.js entry name' do
+      subject { asset_bundle_path('item_group_editor.js') }
+
+      it 'returns actual file name' do
+        is_expected.to eq '/packs/item_group_editor-857e5bfa272e71b6384046f68ba29d44.js'
+      end
+    end
+
+    context 'given existing *.css entry name' do
+      subject { asset_bundle_path('item_group_editor.css') }
+
+      it 'returns actual file name' do
+        is_expected.to eq '/packs/item_group_editor-5d7c7164b8a0a9d675fad9ab410eaa8d.css'
+      end
+    end
+
+    context 'given non-existing entry name' do
+      subject do
+        -> { asset_bundle_path('not_found.js') }
+      end
+
+      it 'raises' do
+        is_expected.to raise_error WebpackBundleHelper::BundleNotFound
+      end
+    end
+  end
+
+  describe '#javascript_bundle_tag' do
+    context 'given existing *.js entry name' do
+      subject { javascript_bundle_tag('item_group_editor') }
+
+      it 'renders a nice <script> tag' do
+        is_expected.to eq '<script src="/packs/item_group_editor-857e5bfa272e71b6384046f68ba29d44.js" defer="defer"></script>'
+      end
+    end
+
+    context 'given existing *.js entry name with async' do
+      subject { javascript_bundle_tag('item_group_editor', async: true) }
+
+      it 'renders a nice <script> tag' do
+        is_expected.to eq '<script src="/packs/item_group_editor-857e5bfa272e71b6384046f68ba29d44.js" async="async"></script>'
+      end
+    end
+
+    context 'given non-existing *.js entry name' do
+      subject do
+        -> { javascript_bundle_tag('not_found') }
+      end
+
+      it 'raises' do
+        is_expected.to raise_error WebpackBundleHelper::BundleNotFound
+      end
+    end
+  end
+
+  describe '#stylesheet_bundle_tag' do
+    context 'given existing *.css entry name' do
+      subject { stylesheet_bundle_tag('item_group_editor') }
+
+      it 'renders a nice <link> tag' do
+        is_expected.to eq '<link rel="stylesheet" media="screen" href="/packs/item_group_editor-5d7c7164b8a0a9d675fad9ab410eaa8d.css" />'
+      end
+    end
+
+    context 'given non-existing *.css entry name' do
+      subject do
+        -> { stylesheet_bundle_tag('not_found') }
+      end
+
+      it 'raises' do
+        is_expected.to raise_error WebpackBundleHelper::BundleNotFound
+      end
+    end
+  end
+
+  describe '#image_bundle_tag' do
+    context 'given existing *.png entry name' do
+      subject { image_bundle_tag('union-ok.png') }
+
+      it 'renders a nice <img> tag' do
+        is_expected.to eq '<img src="/packs/union-ok-857e5bfa272e71b6384046f68ba29d44.png" />'
+      end
+    end
+
+    context 'given non-existing *.png entry name' do
+      subject do
+        -> { image_bundle_tag('not_found.png') }
+      end
+
+      it 'raises' do
+        is_expected.to raise_error WebpackBundleHelper::BundleNotFound
+      end
+    end
+
+    context 'given without extname' do
+      subject do
+        -> { image_bundle_tag('not_found') }
+      end
+
+      it 'raises' do
+        is_expected.to raise_error ArgumentError
+      end
+    end
+  end
+end
