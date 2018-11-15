729a99ea4593aeb06cf7f9de13674ffca0cf97a3
pipopotamasu <ec0156hx@gmail.com>
2018-11-16 00:25:05 +0900

    testing with capybara

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'show sell information', type: :feature, js: true do
     before { visit root_path }
 
     it 'test' do
-      expect(page).to have_content 'test'
+      expect(page).to have_css '.test'
     end
   end
 end
