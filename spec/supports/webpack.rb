38810a1d2570e76decef11703590cc7257212f2d
pipopotamasu <ec0156hx@gmail.com>
2018-11-28 15:38:07 +0900

    run webpack when feature spec

---
+++ spec/supports/webpack.rb
@@ -0,0 +1,10 @@
+RSpec.configure do |config|
+  $_run_webpack = false
+
+  config.before(:context, js: :true) do
+    unless $_run_webpack
+      system('yarn run dev')
+      $_run_webpack = true
+    end
+  end
+ end
