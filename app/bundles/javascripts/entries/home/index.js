82a82e04d88aad57bd72035169cb61c082a475c0
pipopotamasu <ec0156hx@gmail.com>
2019-01-19 22:59:58 +0900

    change home

--- app/bundles/javascripts/entries/home/index.js
+++ app/bundles/javascripts/entries/home/index.js
@@ -1,9 +1,10 @@
 import '@style/entries/home/home.scss';
+import '@image/webpack-logo.svg';
 import Vue from 'vue';
-import HelloVue from '@js/components/HelloVue';
+import HelloMedbear from '@js/components/HelloMedbear';
 import '@js/modules/home/title_writer';
 
-Vue.component('hello-vue', HelloVue);
+Vue.component('hello-medbear', HelloMedbear);
 
 new Vue({
 	el: '#vue-app'
