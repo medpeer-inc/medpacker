72e04cd3794811ec91811cee1aee48f88b5c7aa9
mpg-tomoya-komiyama <tomoya.komiyama@medpeer.co.jp>
2019-05-28 14:52:41 +0900

    babel設定をファイルに切り出す

--- webpack.common.js
+++ webpack.common.js
@@ -1,172 +1,143 @@
-const glob = require("glob");
-const path = require("path");
-const ManifestPlugin = require("webpack-manifest-plugin");
+/* globals __dirname module */
+const glob = require('glob');
+const path = require('path');
+const ManifestPlugin = require('webpack-manifest-plugin');
 // extract css from bundled javascript
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { VueLoaderPlugin } = require('vue-loader')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { VueLoaderPlugin } = require('vue-loader');
+const TARGET_BROWSERS = require('./browsers');
 
 const bundles = path.join(
-  __dirname,
-  "app",
-  "bundles",
-  "javascripts",
-  "entries"
+	__dirname,
+	'app',
+	'bundles',
+	'javascripts',
+	'entries'
 );
 
-const targets = glob.sync(path.join(bundles, "**/*.{js,jsx,ts,tsx}"));
+const targets = glob.sync(path.join(bundles, '**/*.{js,jsx,ts,tsx}'));
 const entry = targets.reduce((entry, target) => {
-  const bundle = path.relative(__dirname, target);
-  const filename = path.relative(bundles, target);
+	const bundle = path.relative(__dirname, target);
+	const filename = path.relative(bundles, target);
 
-  const ext = path.extname(filename);
+	const ext = path.extname(filename);
 
-  return Object.assign({}, entry, {
-    // Input: "application.js"
-    // Output: { "application": "./app/bundles/javascripts/application.js" }
-    [filename.replace(ext, "")]: "./" + bundle
-  });
+	return Object.assign({}, entry, {
+		// Input: "application.js"
+		// Output: { "application": "./app/bundles/javascripts/application.js" }
+		[filename.replace(ext, '')]: './' + bundle
+	});
 }, {});
 
-const TARGET_BROWSERS = [
-  "ie >= 11",
-  "last 2 Edge versions",
-  "last 2 Chrome versions",
-  "last 2 Firefox versions",
-  "last 2 Safari versions",
-  "last 2 ios_saf versions",
-  "last 2 and_chr versions"
-];
-
 module.exports = {
-  entry: entry,
-  output: {
-    filename: "js/[name]-[hash].js",
-    chunkFilename: "js/[name].bundle-[hash].js",
-    path: path.resolve(__dirname, "public", "bundles"),
-    publicPath: "/bundles/"
-  },
-  plugins: [
-    new ManifestPlugin({
-      fileName: "manifest.json",
-      publicPath: "/bundles/",
-      writeToFileEmit: true
-    }),
-    new MiniCssExtractPlugin({
-      filename: "style/[name]-[hash].css",
-      chunkFilename: "style/[name].bundle-[hash].css"
-    }),
-    new VueLoaderPlugin()
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: "vue-loader"
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: "babel-loader",
-            options: {
-              presets: [
-                [
-                  "@babel/preset-env",
-                  {
-                    modules: false,
-                    targets: {
-                      browsers: TARGET_BROWSERS
-                    },
-                    forceAllTransforms: true,
-                    useBuiltIns: "usage",
-                    corejs: 3
-                  }
-                ]
-              ],
-              plugins: []
-            }
-          }
-        ]
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: "css-loader",
-            options: {
-              // css-loaderの前に噛ませるloaderの数
-              importLoaders: 2
-            }
-          },
-          {
-            loader: "postcss-loader",
-            options: {
-              plugins: [
-                require("autoprefixer")(
-                  {
-                    grid: true,
-                    browsers: TARGET_BROWSERS
-                  }
-                ),
-                require("postcss-flexbugs-fixes")
-              ]
-            }
-          },
-          {
-            loader: "sass-loader",
-            options: {}
-          },
-          {
-            loader: 'sass-resources-loader',
-            options: {
-              resources: ['app/bundles/stylesheets/variables.scss']
-            },
-          },
-        ]
-      },
-      {
-        test: /\.(gif|png|jpg|eot|wof|woff|ttf|svg)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "img/[name]-[hash].[ext]"
-            }
-          }
-        ]
-      }
-    ]
-  },
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /node_modules/,
-          name: "vendor",
-          filename: "js/vendor-[hash].js",
-          chunks: "initial",
-          enforce: true
-        }
-      }
-    }
-  },
-  resolve: {
-    alias: {
-      "@js": path.resolve(__dirname, "app/bundles/javascripts"),
-      "@style": path.resolve(__dirname, "app/bundles/stylesheets"),
-      "@image": path.resolve(__dirname, "app/bundles/images"),
-      "vue$": "vue/dist/vue.esm.js"
-    },
-    extensions: [
-      ".js",
-      ".vue"
-    ]
-  },
-  performance: {
-    hints: "warning", // default value
-    maxEntrypointSize: 250000, // default value
-    maxAssetSize: 250000 // default value
-  }
+	entry: entry,
+	output: {
+		filename: 'js/[name]-[hash].js',
+		chunkFilename: 'js/[name].bundle-[hash].js',
+		path: path.resolve(__dirname, 'public', 'bundles'),
+		publicPath: '/bundles/'
+	},
+	plugins: [
+		new ManifestPlugin({
+			fileName: 'manifest.json',
+			publicPath: '/bundles/',
+			writeToFileEmit: true
+		}),
+		new MiniCssExtractPlugin({
+			filename: 'style/[name]-[hash].css',
+			chunkFilename: 'style/[name].bundle-[hash].css'
+		}),
+		new VueLoaderPlugin()
+	],
+	module: {
+		rules: [
+			{
+				test: /\.vue$/,
+				loader: 'vue-loader'
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: [{ loader: 'babel-loader' }]
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+						options: {
+							// css-loaderの前に噛ませるloaderの数
+							importLoaders: 2
+						}
+					},
+					{
+						loader: 'postcss-loader',
+						options: {
+							plugins: [
+								require('autoprefixer')(
+									{
+										grid: true,
+										browsers: TARGET_BROWSERS
+									}
+								),
+								require('postcss-flexbugs-fixes')
+							]
+						}
+					},
+					{
+						loader: 'sass-loader',
+						options: {}
+					},
+					{
+						loader: 'sass-resources-loader',
+						options: {
+							resources: ['app/bundles/stylesheets/variables.scss']
+						},
+					},
+				]
+			},
+			{
+				test: /\.(gif|png|jpg|eot|wof|woff|ttf|svg)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: 'img/[name]-[hash].[ext]'
+						}
+					}
+				]
+			}
+		]
+	},
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+				vendor: {
+					test: /node_modules/,
+					name: 'vendor',
+					filename: 'js/vendor-[hash].js',
+					chunks: 'initial',
+					enforce: true
+				}
+			}
+		}
+	},
+	resolve: {
+		alias: {
+			'@js': path.resolve(__dirname, 'app/bundles/javascripts'),
+			'@style': path.resolve(__dirname, 'app/bundles/stylesheets'),
+			'@image': path.resolve(__dirname, 'app/bundles/images'),
+			'vue$': 'vue/dist/vue.esm.js'
+		},
+		extensions: [
+			'.js',
+			'.vue'
+		]
+	},
+	performance: {
+		hints: 'warning', // default value
+		maxEntrypointSize: 250000, // default value
+		maxAssetSize: 250000 // default value
+	}
 };
