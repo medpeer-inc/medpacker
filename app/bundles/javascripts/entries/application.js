ebe21a119fa805b46dc68da4fb24383ebc4b3cae
mpg-yamato-murakami <yamato.murakami@medpeer.co.jp>
2019-05-29 12:48:54 +0900

    avoid ie error

--- app/bundles/javascripts/entries/application.js
+++ app/bundles/javascripts/entries/application.js
@@ -1,3 +1,6 @@
 import '@style/entries/application.scss';
 // remote: true使う場合コメント外してください。
 // import "rails-ujs";
+
+// なぜかaxiosでPromiseがundefinedになる現象が起こる。axios実行前にPromiseを呼び出すとなぜか回避できるのでやっておく
+Promise
