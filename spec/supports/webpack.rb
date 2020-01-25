be9e196de5669f2547f0c33271f61b5667ef119e
pipopotamasu <ec0156hx@gmail.com>
2020-01-25 13:00:24 +0900

    global variable -> local variable (#85)

--- spec/supports/webpack.rb
+++ spec/supports/webpack.rb
@@ -1,11 +1,10 @@
 RSpec.configure do |config|
-  $_run_webpack = ENV['SKIP_WEBPACK_BUILD'] == 'true'
+  run_webpack = ENV['SKIP_WEBPACK_BUILD'] == 'true'
 
   config.before(:context, js: :true) do
-    unless $_run_webpack
+    unless run_webpack
       system('yarn run dev')
-      $_run_webpack = true
+      run_webpack = true
     end
   end
  end
- 
