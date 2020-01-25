4dfc62b48a2e5b06693a990859b447752a5f3b48
pipopotamasu <ec0156hx@gmail.com>
2020-01-25 13:25:41 +0900

    resolve images

--- app/bundles/javascripts/components/HelloMedbear.vue
+++ app/bundles/javascripts/components/HelloMedbear.vue
@@ -1,11 +1,22 @@
 <template>
-  <div class="hello-medbear" />
+  <div>
+    <div class="hello-medbear" />
+    <img :src="src" alt="Hello Medbear2" />
+    <img src="~@image/medbear.png" alt="Hello Medbear3" />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import img from '@image/medbear.png';
 
-export default Vue.extend({});
+export default Vue.extend({
+  computed: {
+    src () {
+      return img;
+    }
+  }
+});
 </script>
 
 <style lang="scss" scoped>
