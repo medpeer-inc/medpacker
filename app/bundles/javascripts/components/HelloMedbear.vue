dff646152963c4cbafdf1ed38858d9782ee294ef
pipopotamasu <ec0156hx@gmail.com>
2020-01-25 14:25:28 +0900

    Resolve images (#86)
    * resolve images
    
    * direct reading image
    
    * word break
    
    * lint
    
    * fix test
    
    * lint

--- app/bundles/javascripts/components/HelloMedbear.vue
+++ app/bundles/javascripts/components/HelloMedbear.vue
@@ -1,11 +1,23 @@
 <template>
-  <div class="hello-medbear" />
+  <div>
+    <div class="hello-medbear" />
+    <img :src="src" alt="Hello Medbear2" />
+    <!-- wired... -->
+    <img :src="require('@image/medbear.png').default" alt="Hello Medbear3" />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import img from '@image/medbear.png';
 
-export default Vue.extend({});
+export default Vue.extend({
+  computed: {
+    src(): string {
+      return img;
+    }
+  }
+});
 </script>
 
 <style lang="scss" scoped>
